4efe7e43d422882a6348a5010d16f4d7378c6132
dengshuo <dengshuo7412@163.com>
2023-05-23 08:24:37 +0800

    rustup & cargo update

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ fn main() {
         let mut guess = String::new();
 
         io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
     
         let guess:u32 = match guess.trim().parse(){
