fa563fb7074e1f16d93cf8dab5d776e6b70658b0
dengshuo <dengshuo7412@163.com>
2023-06-02 22:28:38 +0800

    move and copy trait

--- src/main.rs
+++ src/main.rs
@@ -45,26 +45,14 @@ fn main() {
     let guess:i32 = "42".parse().expect("not a number");
     println!("the guess :{guess}");
 
-    let float_v = 5.0;
-    let float_v32: f32 = 3.0;
 
     
 }
 
 fn concept()
 {
-    //const THREE_HOURS_IN_SECONDS:u32 = 60 * 60 * 3;
-
-    // shadowing
-
-    let x = 5;
-
-    let x = x +1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is :{x}");
-
-    }
-    println!("The value of x is {x}");
-
+    let s_origin = String::from("hello");
+    let s_copy = s_origin; // move
+    
+    println!("{}",s_copy);
 }
