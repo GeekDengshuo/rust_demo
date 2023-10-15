91da7430cecfaac384e8ffbe551a1fb3443fc122
dengshuo <dengshuo7412@163.com>
2023-10-15 12:16:50 +0800

    cargo and ownership

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,59 @@
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+// crate
+use rand::Rng;
+
 
 
 
 fn main()
 {
-    println!("please guess the number!");
+    //guess_num();
+    ownership();
 
-    println!("please intput the number:");
+}
+fn guess_num(){
 
-    let mut guess = String::new();
+    println!("Guess the number!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("you guessed:{guess}");
+    let random_num = rand::thread_rng().gen_range(1..=100); // low..=high
+  
+    // comparing loop
+    loop {
+
+        println!("please intput the number:");
+
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("you guessed:{guess}");
+
+        //shadowing variable
+        let guess: u32 = guess.trim().parse().expect("please input a number ...");
+
+        match guess.cmp(&random_num){
+            Ordering::Less => println!("too small !"),
+            Ordering::Greater => println!("to big !"),
+            Ordering::Equal => {
+                println!("you win ~"); 
+                break;
+            }
+    
+        }
+    }
+    
+    println!("The random secret number:{random_num}");
+}
+
+fn ownership(){
+    let str1 = String::from("hello world");
+    // let str2 = str1;               // shadow copy will free str1
+    let str2 = str1.clone();  // deep copy
+
+    println!("The string is {str1}");
+
+    
 }
 
